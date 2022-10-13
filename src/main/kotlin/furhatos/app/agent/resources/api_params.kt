1db963c6ccb4406bacf98de41c8e5b871c28f286
ZENBOOK-VAN-JOC\Jochem van Lith <j.a.e.vanlith@student.tudelft.nl>
2022-10-13 23:05:19 +0200

    Add entities for spoonacular params

---
+++ src/main/kotlin/furhatos/app/agent/resources/api_params.kt
@@ -0,0 +1,83 @@
+package furhatos.app.agent.resources
+
+fun getCuisines(): List<String> {
+    return listOf<String>(
+        "African",
+        "American",
+        "British",
+        "Cajun",
+        "Caribbean",
+        "Chinese",
+        "Eastern European",
+        "European",
+        "French",
+        "German",
+        "Greek",
+        "Indian",
+        "Irish",
+        "Italian",
+        "Japanese",
+        "Jewish",
+        "Korean",
+        "Latin American",
+        "Mediterranean",
+        "Mexican",
+        "Middle Eastern",
+        "Nordic",
+        "Southern",
+        "Spanish",
+        "Thai",
+        "Vietnamese"
+    )
+}
+
+fun getDiets(): List<String> {
+    return listOf<String>(
+        "Gluten Free",
+        "Ketogenic",
+        "Vegetarian",
+        "Lacto-Vegetarian",
+        "Ovo-Vegetarian",
+        "Vegan",
+        "Pescetarian",
+        "Paleo",
+        "Primal",
+        "Low FODMAP",
+        "Whole30")
+}
+
+fun getIntolerances(): List<String> {
+    return listOf<String>(
+        "Dairy",
+        "Egg",
+        "Gluten",
+        "Grain",
+        "Peanut",
+        "Seafood",
+        "Sesame",
+        "Shellfish",
+        "Soy",
+        "Sulfite",
+        "Tree Nut",
+        "Wheat"
+    )
+}
+
+fun getMealTypes(): List<String> {
+    return listOf<String>(
+        "main course",
+        "side dish",
+        "dessert",
+        "appetizer",
+        "salad",
+        "bread",
+        "breakfast",
+        "soup",
+        "beverage",
+        "sauce",
+        "marinade",
+        "fingerfood",
+        "snack",
+        "drink"
+    )
+}
