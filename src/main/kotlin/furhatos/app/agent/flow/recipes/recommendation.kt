04ef67d5e0c4a5a9812104dead2299433cb8e222
Jeroen Hofland <hofland.jeroen@gmail.com>
2022-10-14 17:37:44 +0200

    Excluded query and common states/functions

--- src/main/kotlin/furhatos/app/agent/flow/recipes/recommendation.kt
+++ src/main/kotlin/furhatos/app/agent/flow/recipes/recommendation.kt
@@ -5,7 +5,6 @@ import furhatos.app.agent.flow.main.Idle
 import furhatos.nlu.common.*
 import furhatos.nlu.common.Number as NumberNLU
 import furhatos.flow.kotlin.*
-import furhatos.gestures.Gestures
 import kotlin.random.Random
 
 val BASE_URL = "https://api.spoonacular.com" // Spoonacular API url
@@ -127,85 +126,4 @@ fun SearchRecipes(input : String) : State = state(Parent) {
             }
         }
     }
-}
-
-val AcceptFromList: State = state(Parent) {
-    onEntry {
-        furhat.ask("Do you like any of them?")
-    }
-
-    // If the user likes any of them
-    onResponse<Yes>{
-        furhat.ask("Which one do you like?")
-    }
-
-    // If not, ask if the user wants new ones
-    onResponse<No>{
-        furhat.say("I will search for new ones.")
-        terminate("")
-    }
-
-    // If not
-    onResponse{
-        // TODO recognize which one the user likes
-        terminate(it.text)
-    }
-}
-
-// Query to the API
-fun query(user_input: String, query_type: String) = state {
-    onEntry {
-        // While waiting for API
-        furhat.say(async = true) {
-            +"Let's see"
-            +Gestures.GazeAway
-        }
-
-        val base_query = "$BASE_URL/recipes/$query_type?apiKey=${API_KEY}"
-        var result = listOf<String>()
-        when (query_type) {
-            "random" -> {
-                val query = base_query.plus("&number=${user_input}")
-
-                // Get the title of the dish from the response
-                val objects = khttp.get(query).jsonObject.getJSONArray("recipes")
-                for (i in 0 until user_input.toInt()) {
-                    result += (objects.getJSONObject(i).getString("title"))
-                }
-
-                terminate(result)
-            }
-            "search"-> {
-                // Parse the query
-                val question = user_input.replace("+", " plus ").replace(" ", "+")
-                val query = base_query.plus("&query=${question}")
-
-                // Get the title of the dish from the response
-                val objects = khttp.get(query).jsonObject.getJSONArray("results")
-                for (i in 0 until 5) {
-                    result += (objects.getJSONObject(i).getString("title"))
-                }
-
-            }
-            else -> {
-                print("Nothing here yet")
-                // TODO what to do when query_type not defined
-            }
-        }
-
-        // Return the response
-        terminate(result)
-    }
-
-    onTime(TIMEOUT) {
-        // If timeout is reached, we return nothing
-        terminate(emptyList<String>())
-    }
-}
-
-val FoodJoke : State = state(Parent) {
-    onEntry {
-        furhat.ask("Would you like to hear a joke?")
-    }
-    // TODO foodjoke connect to API
 }
