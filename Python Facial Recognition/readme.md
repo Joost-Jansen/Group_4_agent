981214e749d70f0a9d7eb180b0ebde9d3458218d
ZENBOOK-VAN-JOC\Jochem van Lith <j.a.e.vanlith@student.tudelft.nl>
2022-10-24 22:49:42 +0200

    Add recipe recommendation

---
+++ Python Facial Recognition/readme.md
@@ -0,0 +1,18 @@
+**run these scripts:** 
+
+py -3 -m venv .venv
+
+.venv\scripts\activate
+
+python -m pip install --upgrade pip
+
+pip install flask
+
+pip install opencv-contrib-python
+
+pip install dlib
+
+run by:
+
+**python -m flask run**
+
---
+++ src/main/kotlin/furhatos/app/agent/flow/memory/data/LongTermData.kt
@@ -0,0 +1,33 @@
+package furhatos.app.agent.flow.memory.data
+
+
+data class LongTermData(
+    val user_id: Int,
+    var meals: MutableList<Meal>,
+    var ingredients: MutableList<Ingredient>,
+    var cuisines: MutableList<Cuisine>
+)
+data class Meal(
+    val id: Int, // id in spoonacular can request by getID
+    val name: String, // name of meal
+    var ingredients: MutableList<String>,
+    val course: String, // type of meal eg. desert
+    var likes: Int, // amount of likes or dislikes (when negative)
+    var last_selected: String // last time this meal was selected. Needs to be parsed with LocalDate (cannot do it beforehand. Makes difficulties with readinf and writing
+) : Comparable<Meal> {
+    override fun compareTo(other: Meal) = compareValuesBy(this, other) { it.likes }
+}
+
+data class Ingredient(
+    val name: String,
+    var likes: Int
+) : Comparable<Ingredient> {
+    override fun compareTo(other: Ingredient) = compareValuesBy(this, other) { it.likes }
+}
+
+data class Cuisine(
+    val name: String,
+    var likes: Int
+) : Comparable<Cuisine> {
+    override fun compareTo(other: Cuisine) = compareValuesBy(this, other) { it.likes }
+}
